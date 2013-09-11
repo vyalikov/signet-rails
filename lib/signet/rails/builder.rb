0145b3949562d07101950c2731d79e466aae7f72
ilya <vyalikov-sky@mail.ru>
2013-09-11 21:49:35 +0400

    Options refactoring in builder.rb

--- lib/signet/rails/builder.rb
+++ lib/signet/rails/builder.rb
@@ -7,9 +7,20 @@ module Signet
 
       class << self
         attr_accessor :default_options # better than @@ variable
+        default_options = {}
       end
 
-      Builder.default_options = {}
+      BUILTIN_OPTIONS = {
+        persist_attrs: [:refresh_token, :access_token, :expires_in, :issued_at],
+        name: :google,
+        type: :webserver,
+        storage_attr: :signet,
+        approval_prompt: 'auto',
+        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+        handle_auth_callback: true,
+        persistence_wrapper: :active_record
+      }
 
       def self.set_default_options(opts = {})
         # normalize to symbol hash
@@ -21,20 +32,18 @@ module Signet
       end
 
       def provider(opts = {}, &block)
-        # normalize to symbol hash
-        n_opts = opts.symbolize_keys
 
         # use the default_options as a base... then merge these changes on top
-        combined_options = Builder.default_options.merge(n_opts)
+        combined_options = BUILTIN_OPTIONS.merge(Builder.default_options.merge(opts.symbolize_keys));
 
         # now set some defaults if they aren't already set
-        combined_options[:persist_attrs] ||= [:refresh_token, :access_token, :expires_in, :issued_at]
-        combined_options[:name] ||= :google
+        # combined_options[:persist_attrs] ||= [:refresh_token, :access_token, :expires_in, :issued_at]
+        # combined_options[:name] ||= :google
 
         # is this a login-based OAuth2 adapter? If so, the callback will be used to identify a
         # user and create one if necessary
         # Options: :login, :webserver
-        combined_options[:type] ||= :webserver
+        # combined_options[:type] ||= :webserver
 
         # name of hash-behaving attribute on our wrapper that contains credentials
         # keyed by :name
@@ -46,36 +55,30 @@ module Signet
               #     "expires_in": 123
               #   }
               # }
-        combined_options[:storage_attr] ||= :signet
+        # combined_options[:storage_attr] ||= :signet
 
         # TODO: see https://developers.google.com/accounts/docs/OAuth2Login#authenticationuriparameters
-        combined_options[:approval_prompt] ||= 'auto'
+        # combined_options[:approval_prompt] ||= 'auto'
 
         # unless specified, we need to set this at request-time because we need the env to get server etc
         # combined_options[:redirect_uri] = ??? need env
 
         # TODO: better way of sourcing these defaults... from signet?
-        combined_options[:authorization_uri] ||= 'https://accounts.google.com/o/oauth2/auth'
-        combined_options[:token_credential_uri] ||= 'https://accounts.google.com/o/oauth2/token'
+        # combined_options[:authorization_uri] ||= 'https://accounts.google.com/o/oauth2/auth'
+        # combined_options[:token_credential_uri] ||= 'https://accounts.google.com/o/oauth2/token'
 
         # whether we handle the persistence of the auth callback or simply pass-through
-        combined_options[:handle_auth_callback] ||= true
+        # combined_options[:handle_auth_callback] ||= true
 
         # The following lambda will be used when creating a new client in a factory
         # to get the persistence object
+
+
+        # - default options
         combined_options[:extract_from_env] ||= lambda do |env, client|
-
-          session = env['rack.session']
-
-          if session && session[:user_id]
-            begin
-              u = User.find(session[:user_id])
-              u.o_auth2_credentials.where(name: combined_options[:name]).first
-            rescue ActiveRecord::RecordNotFound
-              nil
-            end
-          else
-            nil
+          process_with_user_id(env) do |user_id|
+            u = User.find(user_id)
+            u.o_auth2_credentials.where(name: combined_options[:name]).first
           end
         end
 
@@ -90,26 +93,18 @@ module Signet
               if combined_options[:type] == :login
                 User.first_or_create(uid: "#{combined_options[:name]}_#{id}")
               else
-                session = env['rack.session']
-                if session && session[:user_id]
-                  begin
-                    User.find(session[:user_id])
-                  rescue ActiveRecord::RecordNotFound
-                    nil
-                  end
-                else
-                  fail 'Expected to be able to find user in session'
+                process_with_user_id(env, true) do |user_id|
+                  User.find(user_id)
                 end
               end
 
             u.o_auth2_credentials.first_or_initialize(name: combined_options[:name])
-
           rescue ActiveRecord::RecordNotFound
             nil
           end
         end
 
-        combined_options[:persistence_wrapper] ||= :active_record
+        # combined_options[:persistence_wrapper] ||= :active_record
 
         # define a lambda that returns a lambda that wraps our OAC lambda return object
         # in a persistance object
@@ -138,6 +133,23 @@ module Signet
       def call(env)
         to_app.call(env)
       end
+
+    private
+      def process_with_user_id(env, session_required = false, &block)
+        session = env['rack.session']
+
+        if session && session[:user_id]
+          begin
+            yield(session[:user_id])
+          rescue ActiveRecord::RecordNotFound
+            nil
+          end
+        else
+          fail 'Expected to be able to find user in session' if session_required
+          nil
+        end
+      end
+
     end
   end
 end
