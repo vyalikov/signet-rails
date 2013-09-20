eaced17d61c4ed09daa31dce68d1674223a6c9f5
ilya <vyalikov-sky@mail.ru>
2013-09-20 19:50:48 +0400

    store user data throught wrapper

--- lib/signet/rails/wrappers/active_record.rb
+++ lib/signet/rails/wrappers/active_record.rb
@@ -11,8 +11,32 @@ module Signet
         attr_reader :client
 
         def persist
-          @credentials.save if @credentials.changed?
+          @credentials.save! if @credentials.changed?
         end
+
+        def self.generate_user_uid(id, provider_name)
+          "#{provider_name}_#{id}"
+        end
+
+        # should return [USER] abstract structure 
+        def self.first_or_create_user(uid, provider_name)
+          User.first_or_create(uid: generate_user_uid(uid, provider_name) )
+        end
+
+        # should return [USER] abstract structure
+        def self.get_user_by_id(id)
+          User.find(id)
+        end
+
+        # user is [USER] abstract structure - returns [CREDENTIAL] structure
+        def self.get_user_credentials(user, provider_name)
+          user.o_auth2_credentials.where(name: provider_name).first # activerecord table structure
+        end
+
+        def self.get_or_initialize_user_credentials(user, provider_name)
+          user.o_auth2_credentials.first_or_create(name: provider_name)
+        end
+
       end
     end
   end
